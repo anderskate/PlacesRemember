8c89ad102c6269d55931aad7aeb64b9610aa6309
anderskate <andrey.starozhilov@mail.ru>
2019-11-10 14:40:25 +0500

    Add user selection in Place model

--- mysite/visited_places_app/models.py
+++ mysite/visited_places_app/models.py
@@ -1,7 +1,12 @@
 from django.contrib.gis.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Place(models.Model):
     name = models.CharField('Название', max_length=40)
     location = models.PointField(srid=4326)
     comment = models.TextField('Комментарий о месте')
+    author = models.ForeignKey(User, default=1, verbose_name='пользователь', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
